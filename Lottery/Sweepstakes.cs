ff336552e94289145bdbbd9de7162bac17f93a97
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-01-31 06:35:04 -0800

    Added all necessary classes

---
+++ Lottery/Sweepstakes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    class Sweepstakes
+    {
+
+        //Member Variables (HAS A)
+
+        //Constructor
+
+        //Member Methods (CAN DO)
+        public void SweepstakesMethod(string name)
+        {
+
+        }
+
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+        public Contestant PickWinner()
+        {
+            Contestant contestant;
+
+            return contestant;
+        }
+
+        public void PrintcontestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
